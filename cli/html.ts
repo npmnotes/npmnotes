360d29efc86d15991ac46e788ca5c08c7e691aa7
Gilad Kutiel <gilad.kutiel@gmail.com>
2018-08-16 00:06:23 +0300

    moved script into body to allow devd live reload

--- cli/html.ts
+++ cli/html.ts
@@ -16,11 +16,11 @@ export default function html(data: {
 <link rel="shortcut icon"type="image/x-icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAA9GAAAPRgFoUyCCAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAFpQTFRF////2FlKykY4wDorykg4zsmuz8qv11pK2dS92nFh23dn29fB3YNz46iY46ub4+DM5ODN5eLP5r+v58Ky5+PR6OTS6ebU6ebV6ufW6+fX6+jX7ObW7OnZ7erabqlTygAAAAN0Uk5TAEh8m2U8xAAAAGlJREFUWIXt0LcNwDAMBVE655zT/mu6sBsCBCw1tIp/A7ziiJ784C1MzPKIBwCAo0B/yFWmwHDJ1arAmPJWW6CJeLMtMBW8Xf3B1vFO9QdtzFvUH/wL5KVcZgp8BQAAAAAAAAAAAMBl4AYolF9xk2tqiAAAAABJRU5ErkJggg==" />
 <style>${data.css}</style>
 <script>const data = ${JSON.stringify(data.notes)}</script>
-<script type="module">${data.js}</script>
 <title>${data.title}</title>
 </head>
 
 <body>
+<script type="module">${data.js}</script>
 <input id='dummy-input' type="text">
 
 
